ea6dc6a440906fcb6f4c6e4e4d06f6fd65f693d1
Faisal Sayed <faisal.sayed502@gmail.com>
2024-05-18 23:05:25 -0400

    stuff

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -16,7 +16,7 @@ function MainPage() {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setAllAboard(true);
-    }, 2000);
+    }, 5000);
 
     return () => clearTimeout(timeout);
   }, []);
@@ -49,131 +49,184 @@ function MainPage() {
 
   return (
     <>
-      <div key="page-4">
-        <div>
+      <div className="grain" />
+      <div className="flex items-center justify-center h-screen sm:hidden">
+        <div className="text-center py-5 border-2 border-black pl-20 pr-20">
+          Rotate your device<br></br>to landscape
+        </div>
+      </div>
+      <main className="hidden sm:block">
+        <div className="min-h-screen">
           <img src="/train.png" className="w-[80%] my-10 mx-auto block" draggable="false" />
+          {allAboard && (
+            <motion.div
+              className="lg:-translate-y-20 md:-translate-y-16 sm:-translate-y-14 -translate-y-12"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+              <h1 className="text-[#626543] stroke lg:text-[44px] font-Neela text-center md:text-3xl sm:text-xl mb-2">
+                THE BOREAL EXPRESS
+              </h1>
+              <p className="text-[#626543] stroke lg:text-3xl font-Neela text-center md:text-xl sm:text-lg mb-1">
+                HACK CLUB’S CROSS-COUNTRY HACKATHON
+              </p>
+              <p className="text-[#626543] stroke lg:text-2xl font-Neela text-center md:text-lg sm:text-base">
+                JULY 21-28 • VANCOUVER - MONTREAL
+              </p>
+            </motion.div>
+          )}
         </div>
-        {allAboard && (
-          <motion.div
-            className="lg:-translate-y-20 md:-translate-y-16 sm:-translate-y-14 -translate-y-12"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1 }}
-          >
-            <h1 className="text-[#626543] stroke lg:text-5xl font-Neela text-center md:text-3xl sm:text-xl">
-              THE BOREAL EXPRESS
+
+        <div className="flex items-center justify-center">
+          <div className="p-20 w-[60%] bg-[#D9D9D9] rounded-lg border-[3px] border-[#6D5C55] border-dashed">
+            <h1 className="text-[#626543] stroke lg:text-3xl font-Neela md:text-3xl sm:text-xl mb-2">
+              DEAR HACKER,
             </h1>
-            <p className="text-[#626543] stroke lg:text-3xl font-Neela text-center md:text-xl sm:text-lg">
-              HACK CLUB’S CROSS-COUNTRY HACKATHON
+            <br />
+            <p>Every summer Hack Clubbers do something special.</p>
+            <br />
+            <p className="text-justify">
+              We travel across the world to attend hackathons, run virtual events worldwide, and
+              spend hours building projects we care about. All for one goal: to build the world we
+              want to live in.
             </p>
-            <p className="text-[#626543] stroke lg:text-2xl font-Neela text-center md:text-lg sm:text-base">
-              JULY 21-28 • VANCOUVER - MONTREAL
+            <br />
+            <p className="font-bold text-justify">
+              This year, join us on our next great voyage: A Transcontinental Hackathon across
+              Canada.
             </p>
-          </motion.div>
-        )}
-      </div>
-      <div className="w-full py-20">
-        <img src="/poster2.svg" className="block mx-auto max-w-[80%]" />
-      </div>
-      <div className="bg-[#C3C6A1] w-full py-20 mt-10">
-        <div className="font-Neela text-[#6D5C55] text-[80px] bg-[#C3C6A1] text-center">
-          Register your interest
-        </div>
-        <a href="https://forms.hackclub.com/t/dXZwSPpG1Sus">
-          <motion.img
-            whileHover={{ scale: 1.1 }}
-            src="/ticket.png"
-            alt="ticket"
-            draggable="false"
-            className="mx-auto mt-20"
-          />
-        </a>
-      </div>
-      <div key="page-3" className="pt-10 flex items-center justify-between min-h-screen flex-col">
-        <div className="sm:flex space-y-10 gap-12 mb-10 max-w-[80%]">
-          <div className="mt-10">
-            <div className="font-Neela sm:text-[55px] brown-stroke text-[#626543]">
-              What will you do
-              <br />
-              with your time?
-            </div>
-            <div className="text-[26px] font-bold">7 Nights • 8 Days • 50 Hackers</div>
-
-            <div className="mt-8">
-              <img src="/route2.svg" draggable="false" />
-            </div>
-          </div>
-          <div className="space-y-10">
-            <div>
-              <img src="/z1.svg" />
-            </div>
-
-            <div>
-              <img src="/z2.svg" />
-            </div>
+            <br />
+            <p className="text-justify">
+              This is not a regular hackathon. It's not summer camp. This is a journey for you to
+              discover new ways of living life. Discover what the world has to offer.
+            </p>
+            <br />
+            <p className="text-justify">
+              On July 21, the night of the full moon, we start our 8-day expedition in Vancouver.
+              We'll go through the Canadian Rockies, passing through Jasper, Winnipeg, and Toronto,
+              concluding in Montreal.
+            </p>
+            <br />
+            <p>
+              Together, we will travel through one of the most pristine remaining lands on earth,
+              only accessible by rail. Onboard the train, you would build projects alongside some of
+              the most kickass engineers, artists, hackers, and scientists working on highly
+              technical solutions to save the environment.
+            </p>
+            <br />
+            <p className="text-justify">
+              This is your invitation to build the things you care about and see your actions change
+              the world around you. Join us aboard The Boreal Express.
+            </p>
+            <br />
+            <p>Warmly,</p>
+            <br />
+            <br />
+            <br />
           </div>
         </div>
-      </div>
-      <div className="bg-[#AAAD83] py-5 px-20">
-        <div className="max-w-[80%] mx-auto">
-          <div className="mt-10">
-            <div className="font-Neela text-[55px] brown-stroke text-[#6D5C55]">
-              Experiences that linger<br></br>often forever...
-            </div>
-            <div className="text-[26px]">
-              A Glimpse of what your journey onboard The Boreal Express would look like
-            </div>
+
+        <div className="bg-[#C3C6A1] w-full py-20 mt-10">
+          <div className="font-Neela text-[#6D5C55] text-[80px] bg-[#C3C6A1] text-center">
+            Register your interest
           </div>
-          <div className="flex justify-center items-stretch mt-8 gap-10">
-            <div className="flex-1 flex flex-col">
-              <img src="/rock.svg" className="rounded-xl flex-1" />
-              <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
-                The Canadian Rockies
+          <a href="https://forms.hackclub.com/t/dXZwSPpG1Sus">
+            <motion.img
+              whileHover={{ scale: 1.1 }}
+              src="/ticket.png"
+              alt="ticket"
+              draggable="false"
+              className="mx-auto mt-20"
+            />
+          </a>
+        </div>
+        <div key="page-3" className="pt-10 flex items-center justify-between min-h-screen flex-col">
+          <div className="sm:flex space-y-10 gap-12 mb-10 max-w-[80%]">
+            <div className="mt-10">
+              <div className="font-Neela sm:text-[55px] brown-stroke text-[#626543]">
+                What will you do
+                <br />
+                with your time?
+              </div>
+              <div className="text-[26px] font-bold">7 Nights • 8 Days • 50 Hackers</div>
+
+              <div className="mt-8">
+                <img src="/route2.svg" draggable="false" />
               </div>
             </div>
-
-            <div className="flex-1 flex flex-col">
-              <img src="/jasper.webp" className="rounded-xl flex-1" />
-              <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
-                Jasper, Alberta
+            <div className="space-y-10">
+              <div>
+                <img src="/z1.svg" />
               </div>
-            </div>
 
-            <div className="flex-1 flex flex-col">
-              <img src="/vancouver.png" className="rounded-xl flex-1" />
-              <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
-                Vancouver, BC
-              </div>
-            </div>
-          </div>
-
-          <div className="flex justify-center mt-20 gap-10 pb-[5rem]">
-            <div className="flex-1 flex flex-col">
-              <img src="/toronto.svg" className="rounded-xl flex-1" />
-              <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
-                Toronto, Ontario
-              </div>
-            </div>
-
-            <div className="flex-1 flex flex-col">
-              <img src="/winnipeg.svg" className="rounded-xl flex-1" />
-              <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
-                Winnipeg, Manitoba
-              </div>
-            </div>
-
-            <div className="flex-1 flex flex-col">
-              <img src="/rooms.png" className="rounded-xl flex-1" />
-              <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
-                Sleeper Car
+              <div>
+                <img src="/z2.svg" />
               </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className="bg-[#AAAD83] py-5 px-20">
+          <div className="max-w-[80%] mx-auto">
+            <div className="mt-10">
+              <div className="font-Neela text-[55px] brown-stroke text-[#6D5C55]">
+                Experiences that linger<br></br>often forever...
+              </div>
+              <div className="text-[26px]">
+                A Glimpse of what your journey onboard The Boreal Express would look like
+              </div>
+            </div>
+            <div className="flex justify-center items-stretch mt-8 gap-10">
+              <div className="flex-1 flex flex-col">
+                <img src="/rock.svg" className="rounded-xl flex-1" />
+                <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
+                  The Canadian Rockies
+                </div>
+              </div>
 
-      <Faq />
-      <Footer />
+              <div className="flex-1 flex flex-col">
+                <img src="/jasper.webp" className="rounded-xl flex-1" />
+                <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
+                  Jasper, Alberta
+                </div>
+              </div>
+
+              <div className="flex-1 flex flex-col">
+                <img src="/vancouver.png" className="rounded-xl flex-1" />
+                <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
+                  Vancouver, BC
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-center mt-20 gap-10 pb-[5rem]">
+              <div className="flex-1 flex flex-col">
+                <img src="/toronto.svg" className="rounded-xl flex-1" />
+                <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
+                  Toronto, Ontario
+                </div>
+              </div>
+
+              <div className="flex-1 flex flex-col">
+                <img src="/winnipeg.svg" className="rounded-xl flex-1" />
+                <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
+                  Winnipeg, Manitoba
+                </div>
+              </div>
+
+              <div className="flex-1 flex flex-col">
+                <img src="/rooms.png" className="rounded-xl flex-1" />
+                <div className="text-center pt-5 text-[20px] text-[#6D5C55] font-bold">
+                  Sleeper Car
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Faq />
+        <Footer />
+      </main>
     </>
   );
 }
